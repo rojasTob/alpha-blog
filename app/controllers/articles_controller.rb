521fd69606f8f2bebc22e5f074264eb7dee3ad22
Angie Rojas <nataliart4@gmail.com>
2023-12-20 16:29:08 -0500

    Add route to create an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,15 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description)) #this is the way to get the params because is not permited to take them directly from the params obj
+    @article.save
+    redirect_to article_path(@article) #shorten path is redirect_to @article
+  end
+
 end
