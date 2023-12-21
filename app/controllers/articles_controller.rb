abd76886b51824b8c48ec15c7cc8fe4871f7d9e1
Angie Rojas <nataliart4@gmail.com>
2023-12-21 09:22:36 -0500

    Add routes to edit and update an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description)) #this is the way to get the params because is not permited to take them directly from the params obj
     if @article.save
@@ -19,6 +23,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article) #shorten path is redirect_to @article
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
