573d0cee467181cd86029999a5c9e0129a11c906
Angie Rojas <nataliart4@gmail.com>
2023-12-21 09:00:23 -0500

    Display error messages and add flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,17 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description)) #this is the way to get the params because is not permited to take them directly from the params obj
-    @article.save
-    redirect_to article_path(@article) #shorten path is redirect_to @article
+    if @article.save
+      flash[:notice] = "Article was created successfully" # to create a kind of message to show in other page
+      redirect_to article_path(@article) #shorten path is redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 end
