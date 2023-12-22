9773e0b56f2882440516aaccb53bddc0852a4a6e
Angie Rojas <nataliart4@gmail.com>
2023-12-22 16:19:57 -0500

    Add sign up

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
